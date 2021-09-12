9df3777715bdbbfc9d5e2008e7ade136175c0554
Gero-7 <geronimosalaiba@gmail.com>
2021-09-12 17:29:25 -0300

    first

---
+++ Clase 1/index.js
@@ -0,0 +1,15 @@
+let nam = prompt("Ingrese su nombre");
+
+let lastName = prompt("Ingrese su apellido");
+
+let fullName = nam + " " + lastName;
+
+console.log("Su nombre completo es: " + fullName);
+
+let enterNumber = parseFloat(prompt("Ingrese un valor"));
+
+let enterNumberTwo = parseInt(prompt("Ingrese otro valor"));
+
+let sumOfNumbers = enterNumber + enterNumberTwo;
+
+console.log("La suma de los valores es: " + parseFloat(sumOfNumbers));

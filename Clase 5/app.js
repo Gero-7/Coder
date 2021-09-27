c54b6c3e90b6af858df928c28498b2b77f1310d3
Gero-7 <geronimosalaiba@gmail.com>
2021-09-27 16:23:44 -0300

    m

--- Clase 5/app.js
+++ Clase 5/app.js
@@ -1,18 +1,12 @@
-class Producto {
-    constructor(nombre, precio) {
-        this.nombre = nombre.toUpperCase();
-        this.precio = parseFloat(precio);
-        this.vendido = false;
-    }
-    sumaIva() {
-        this.precio = this.precio * 1.21;
-    }
-    vender() {
-        this.vendido = true;
+class Product {
+    constructor(name, price, img) {
+        this.name = name;
+        this.price = price;
+        this.img = img;
     }
 }
-const producto1 = new Producto("arroz", "125");
-const producto2 = new Producto("fideo", "50");
-producto1.sumaIva();
-producto2.sumaIva();
-producto1.vender();
+const produt1 = new Product(
+    "Nike Air Force",
+    15000,
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftiendamia.com%2Fproducto%3Famz%3DB001NGKPEQ&psig=AOvVaw0gTYCfvc04Fh9UivSrcDvq&ust=1632856227888000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLj1zc3tn_MCFQAAAAAdAAAAABAD"
+);

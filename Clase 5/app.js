cd2262f26f65b412075d3e66f1aab5f1a7e69538
Gero-7 <geronimosalaiba@gmail.com>
2021-09-27 16:26:53 -0300

    m

--- Clase 5/app.js
+++ Clase 5/app.js
@@ -4,9 +4,13 @@ class Product {
         this.price = price;
         this.img = img;
     }
+    vender() {
+        this.vendido = true;
+    }
 }
 const produt1 = new Product(
     "Nike Air Force",
     15000,
     "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftiendamia.com%2Fproducto%3Famz%3DB001NGKPEQ&psig=AOvVaw0gTYCfvc04Fh9UivSrcDvq&ust=1632856227888000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLj1zc3tn_MCFQAAAAAdAAAAABAD"
 );
+produt1.vender();
